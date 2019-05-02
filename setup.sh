329747a541aed3333bf4f1e7aba40fa5daa21c01
Lilan Madhusankha <3xtr3m3r89@gmail.com>
2019-05-03 00:00:51 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@ export C_ADMIN_USER="cassandraUser"
 export C_ADMIN_PASS="cassandraPassword"
 export C_CQLSH_SCRIPT="https://s3-us-west-2.amazonaws.com/cassandra-temp-cql-files/input.cql"
 
-source $C_CQLSH_SCRIPT
+wget ${C_CQLSH_SCRIPT}
+service cassandra start
+source input.cql
